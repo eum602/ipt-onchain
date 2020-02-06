32ee8106cdd76c3febc9574935f75a87a79bc9be
Erick <eum602@gmail.com>
2020-02-06 16:27:03 -0500

    added code to read onchain logs periodically

--- pkg/onchain/main.go
+++ pkg/onchain/main.go
@@ -3,26 +3,52 @@ package readsm
 import (
 	"context"
 	"fmt"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/ethclient"
 	readni "ipt-onchain/pkg/onchain/node-ingress"
 	readnr "ipt-onchain/pkg/onchain/node-rules"
 	"log"
 	"math/big"
 	"os"
 	"strconv"
+	"strings"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-var rpcURL string = os.Getenv("RPC_URL")
+var rpcHTTPURL string = os.Getenv("RPC_HTTP_URL")
+var rpcWSURL string = os.Getenv("RPC_WS_URL")
 var addr string = os.Getenv("NODE_INGRESS_CONTRACT_ADDRESS")
+var from *big.Int = getHead()
 
-//Readsm ...
-func Readsm(c chan<- string) {
-	client, err := ethclient.Dial(rpcURL)
+var delayToReadSM time.Duration = 60
+var delayToCheckNewConnections time.Duration = 30
+
+type en struct {
+	EnodeHigh [32]byte
+	EnodeLow  [32]byte
+	Ip        [16]byte
+	Port      uint16
+}
+
+func getHead() *big.Int {
+	client, _ := getClientAndOpts(rpcHTTPURL)
+	header, err := client.HeaderByNumber(context.Background(), nil)
 	if err != nil {
-		fmt.Println("Error", err)
+		log.Fatal(err)
+	}
+	return header.Number
+}
+
+func getClientAndOpts(rpc string) (*ethclient.Client, bind.CallOpts) {
+	client, err := ethclient.Dial(rpc)
+	if err != nil {
+		log.Fatal("Error:", err)
 		os.Exit(0)
 	}
 
@@ -38,14 +64,17 @@ func Readsm(c chan<- string) {
 		From:    auth.From,
 		Context: ctx,
 	}
+	return client, opts
+}
 
+func getNodeRulesContractAddress() common.Address {
+	client, opts := getClientAndOpts(rpcHTTPURL)
 	fmt.Println("Instantiating Node Ingress contract")
 	instContract, err := readni.NewNodeIngressCaller(common.HexToAddress(addr), client)
 	if err != nil {
 		log.Fatalf("could not instantiate contract: %v", err)
 	}
 
-	//GetContractAddress of deployed NodeRules
 	fmt.Println("Getting Noderules contract address")
 	str := "rules"
 	var rules [32]byte
@@ -55,6 +84,13 @@ func Readsm(c chan<- string) {
 	if err != nil {
 		log.Fatalf("could not instantiate contract: %v", err)
 	}
+
+	return nodeRulesContractAddress
+}
+
+func createNodeRulesCallerSessionInstance() readnr.NodeRulesCallerSession {
+	client, opts := getClientAndOpts(rpcHTTPURL)
+	nodeRulesContractAddress := getNodeRulesContractAddress()
 
 	fmt.Println("Creating NodeRules instance")
 	instNodeRules, err := readnr.NewNodeRulesCaller(nodeRulesContractAddress, client)
@@ -67,6 +103,14 @@ func Readsm(c chan<- string) {
 		CallOpts: opts,
 	}
 
+	return nodeRulesCallerSessionInstance
+}
+
+//Readsm ...
+func Readsm() <-chan string {
+	c := make(chan string)
+	nodeRulesCallerSessionInstance := createNodeRulesCallerSessionInstance()
+
 	fmt.Println("Interacting with NodeRules contract adddress...")
 	len, err := nodeRulesCallerSessionInstance.GetSize()
 	currentNodes := int(len.Uint64())
@@ -74,25 +118,104 @@ func Readsm(c chan<- string) {
 		log.Fatalf("could not instantiate contract: %v", err)
 	}
 
-	for i := 0; i < currentNodes; i++ {
-		fmt.Println("Getting Enode", i+1)
-		recenode, err := nodeRulesCallerSessionInstance.GetByIndex(big.NewInt(int64(i)))
-		if err != nil {
-			log.Fatalf("could not instantiate contract: %v", err)
+	go func() {
+		for {
+			for i := 0; i < currentNodes; i++ {
+				fmt.Println("Getting Enode", i+1)
+				recenode, err := nodeRulesCallerSessionInstance.GetByIndex(big.NewInt(int64(i)))
+				if err != nil {
+					log.Fatalf("could not instantiate contract: %v", err)
+				}
+
+				ip := getIP(recenode.Ip)
+				c <- ip
+			}
+			//close(c)
+			fmt.Println("Waiting for: ", delayToReadSM, "minutes")
+			time.Sleep(delayToReadSM * time.Minute)
 		}
+	}()
 
-		ip := getIP(recenode.Ip)
-		c <- ip
-	}
-
-	close(c)
+	return c
 }
 
-type en struct {
-	EnodeHigh [32]byte
-	EnodeLow  [32]byte
-	Ip        [16]byte
-	Port      uint16
+//GetAddedNodeLogs ...
+func GetAddedNodeLogs() <-chan string {
+	c := make(chan string)
+
+	nodeRulesAbi, err := abi.JSON(strings.NewReader(string(readnr.NodeRulesABI)))
+	if err != nil {
+		log.Fatal("Error while getting the ABI for nodeRules", err)
+	}
+
+	client, _ := getClientAndOpts(rpcWSURL)
+	nodeRulesContractAddress := getNodeRulesContractAddress()
+
+	ctx := context.Background()
+
+	go func() {
+		for {
+			// fmt.Println("Waiting for: ",delayToCheckNewConnections,"minutes")
+			// time.Sleep(delayToCheckNewConnections * time.Second)
+			header := getHead()
+
+			fmt.Println("header is", header.String())
+
+			query := ethereum.FilterQuery{
+				FromBlock: from,
+				ToBlock:   header,
+				Addresses: []common.Address{
+					nodeRulesContractAddress,
+				},
+			}
+			logs, err := client.FilterLogs(ctx, query)
+			if err != nil {
+				log.Fatal("Error while getting logs", err)
+			}
+
+			from = header //updating for next reading
+
+			fmt.Println("logs are", logs)
+
+			for _, vLog := range logs {
+				nodeAddedEvent := struct {
+					NodeAdded bool
+					EnodeHigh [32]byte
+					EnodeLow  [32]byte
+					EnodeIp   [16]byte
+					EnodePort uint16
+					Raw       types.Log // Blockchain specific contextual infos
+				}{}
+				err := nodeRulesAbi.Unpack(&nodeAddedEvent, "NodeAdded", vLog.Data)
+				if err != nil {
+					log.Fatal("Error while decoding", err)
+				}
+
+				///////////Identifying the log has the right signature////////////
+				eventSignature := []byte("NodeAdded(bool,bytes32,bytes32,bytes16,uint16)")
+				hash := crypto.Keccak256Hash(eventSignature)
+				fmt.Println("hash.hex", hash.Hex())
+				var topics [4]string
+				for i := range vLog.Topics {
+					topics[i] = vLog.Topics[i].Hex()
+				}
+
+				fmt.Println("topic received", topics[0])
+
+				ip := getIP(nodeAddedEvent.EnodeIp)
+				fmt.Println("received ip is", ip, nodeAddedEvent.NodeAdded)
+				if topics[0] == hash.Hex() {
+					fmt.Println("Node will be added")
+					c <- ip
+				}
+			}
+			// close(c)
+			fmt.Println("Waiting .... ")
+			time.Sleep(delayToCheckNewConnections * time.Second)
+		}
+	}()
+
+	return c
 }
 
 func getEnode(recenode en) string {
